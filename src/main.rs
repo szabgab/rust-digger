f745c787372c6986b8cda0531621249fb8b6d7bb
Gabor Szabo <gabor@szabgab.com>
2023-06-20 09:10:58 +0300

    generate page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rust Digger");
+
+    // Create a folder _site
+    let _res = fs::create_dir_all("_site");
+
+    // Create an html page _site/index.html with the title
+    let filename = "_site/index.html";
+    let mut file = File::create(filename).unwrap();
+    let html = "<html><head><title>Rust Digger</title><body><h1>Rust Digger</h1></body></html>";
+    writeln!(&mut file, "{html}").unwrap();
 }
