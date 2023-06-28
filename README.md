ceb667bdd8dd982e86d82e98adcd3ae41a84f979
Gabor Szabo <gabor@szabgab.com>
2023-06-28 18:54:12 +0300

    start using handlenbars and chrono

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ As of 2023.06.17
 
 1. The git repository https://github.com/rust-lang/crates.io-index does not contain the meta data, such as the github URL
 1. The https://static.crates.io/db-dump.tar.gz is 305 Mb It unzipped to a folder called `2023-06-16-020046` which is 1.1 Gb and contains CSV dumps of a Postgresql database
+
+
+## Local development environment
+
+```
+git clone https://github.com/szabgab/rust-digger.git
+cd rust-digger
+cargo run
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::io::Read;
+
+use chrono::prelude::*;
+use handlebars::Handlebars;
+use serde_json::json;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    println!("Rust Digger");
-
+    println!("Starting the Rust Digger");
     generate_pages();
+    println!("Ending the Rust Digger");
 }
 
 fn generate_pages() {
+    let reg = Handlebars::new();
+
+    let utc: DateTime<Utc> = Utc::now();
+
+    let template_file = "templates/index.html";
+    let mut template = String::new();
+    match File::open(template_file) {
+        Ok(mut file) => {
+            file.read_to_string(&mut template).unwrap();
+        },
+        Err(error) => {
+            println!("Error opening file {}: {}", template_file, error);
+        },
+    }
+
     // Create a folder _site
     let _res = fs::create_dir_all("_site");
 
     // Create an html page _site/index.html with the title
     let filename = "_site/index.html";
     let mut file = File::create(filename).unwrap();
-    let html = format!("
-        <html>
-            <head>
-               <title>Rust Digger</title>
-            </head>
-            <body>
-              <h1>Rust Digger</h1>
-              Version {VERSION}
-              <a href=\"https://github.com/szabgab/rust-digger\">source</a>
-            </body>
-        </html>");
 
-    writeln!(&mut file, "{html}").unwrap();
+    //println!("{VERSION}");
+    let res = reg.render_template(&template, &json!({"version": format!("{VERSION}"), "utc": format!("{}", utc)}));
+    match res {
+        Ok(html) => writeln!(&mut file, "{:?}", html).unwrap(),
+        Err(error) => println!("{}", error)
+    }
 }
+
+
