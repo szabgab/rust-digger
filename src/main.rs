7e8ee53d989e65e1539d909c781ca84d99000ac4
Gabor Szabo <gabor@szabgab.com>
2023-07-29 19:25:32 +0300

    move read_crate_owners to separate file

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 const PAGE_SIZE: usize = 100;
 
 mod read;
-use read::read_users;
+use read::{read_crate_owners, read_users};
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct Repo {
@@ -652,45 +652,6 @@ fn render_news_pages() {
     //            },
     //        }
     //    }
-}
-
-fn read_crate_owners(limit: i32) -> (Owners, CratesByOwner) {
-    //crate_id,created_at,created_by,owner_id,owner_kind
-    let mut owner_by_crate_id: Owners = HashMap::new();
-    let mut crates_by_owner: CratesByOwner = HashMap::new();
-    let filepath = "data/data/crate_owners.csv";
-    log::info!("Start reading {}", filepath);
-    let mut count = 0;
-    match File::open(filepath.to_string()) {
-        Ok(file) => {
-            let mut rdr = csv::Reader::from_reader(file);
-            for result in rdr.deserialize() {
-                count += 1;
-                if limit > 0 && count >= limit {
-                    log::info!("Limit of {limit} reached");
-                    break;
-                }
-                let record: CrateOwner = match result {
-                    Ok(value) => value,
-                    Err(error) => panic!("Error {}", error),
-                };
-                owner_by_crate_id.insert(record.crate_id.clone(), record.owner_id.clone());
-                crates_by_owner
-                    .entry(record.owner_id.clone())
-                    .or_insert(vec![]);
-                let _ = &crates_by_owner
-                    .get_mut(&record.owner_id)
-                    .unwrap()
-                    .push(record.crate_id.clone());
-                //dbg!(&crates_by_owner[&record.owner_id]);
-            }
-        }
-        Err(error) => panic!("Error opening file {}: {}", filepath, error),
-    }
-
-    log::info!("Finished reading {filepath}");
-
-    (owner_by_crate_id, crates_by_owner)
 }
 
 fn read_crates(limit: i32) -> Vec<Crate> {
--- src/read.rs
+++ src/read.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 
-use crate::{User, Users};
+use crate::{CrateOwner, CratesByOwner, Owners, User, Users};
 
 pub fn read_users(limit: i32) -> Users {
     let mut users: Users = HashMap::new();
@@ -29,4 +29,43 @@ pub fn read_users(limit: i32) -> Users {
 
     log::info!("Finished reading {filepath}");
     users
+}
+
+pub fn read_crate_owners(limit: i32) -> (Owners, CratesByOwner) {
+    //crate_id,created_at,created_by,owner_id,owner_kind
+    let mut owner_by_crate_id: Owners = HashMap::new();
+    let mut crates_by_owner: CratesByOwner = HashMap::new();
+    let filepath = "data/data/crate_owners.csv";
+    log::info!("Start reading {}", filepath);
+    let mut count = 0;
+    match File::open(filepath.to_string()) {
+        Ok(file) => {
+            let mut rdr = csv::Reader::from_reader(file);
+            for result in rdr.deserialize() {
+                count += 1;
+                if limit > 0 && count >= limit {
+                    log::info!("Limit of {limit} reached");
+                    break;
+                }
+                let record: CrateOwner = match result {
+                    Ok(value) => value,
+                    Err(error) => panic!("Error {}", error),
+                };
+                owner_by_crate_id.insert(record.crate_id.clone(), record.owner_id.clone());
+                crates_by_owner
+                    .entry(record.owner_id.clone())
+                    .or_insert(vec![]);
+                let _ = &crates_by_owner
+                    .get_mut(&record.owner_id)
+                    .unwrap()
+                    .push(record.crate_id.clone());
+                //dbg!(&crates_by_owner[&record.owner_id]);
+            }
+        }
+        Err(error) => panic!("Error opening file {}: {}", filepath, error),
+    }
+
+    log::info!("Finished reading {filepath}");
+
+    (owner_by_crate_id, crates_by_owner)
 }
