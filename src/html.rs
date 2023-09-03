40cc90565bc5483765110fe88b15d17f9bd27e1c
Gabor Szabo <gabor@szabgab.com>
2023-09-03 14:03:24 +0300

    format

--- src/html.rs
+++ src/html.rs
@@ -10,9 +10,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 const PAGE_SIZE: usize = 100;
 
 mod common;
-use common::{
-    load_details, percentage, Crate, CratesByOwner, Owners, Repo, User,
-};
+use common::{load_details, percentage, Crate, CratesByOwner, Owners, Repo, User};
 mod read;
 use read::{read_crate_owners, read_crates, read_teams, read_users};
 mod render;
