2387722e1b24b0466eb530da7fdcec3fe21f23c9
Gabor Szabo <gabor@szabgab.com>
2024-03-13 19:33:41 +0200

    update year

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@ rm -f db-dump.tar.gz
 rm -frd data/
 time wget --quiet https://static.crates.io/db-dump.tar.gz
 time tar xzf db-dump.tar.gz
-DIR=$(ls -d1 2023-* | head -n 1 | cut -d'/' -f1)
+DIR=$(ls -d1 2024-* | head -n 1 | cut -d'/' -f1)
 mv "$DIR" "data"
