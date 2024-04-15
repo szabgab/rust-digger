ee4c59ad41572c403a25fbdf313d758e9f5d3957
Gabor Szabo <gabor@szabgab.com>
2024-04-15 20:40:40 +0300

    add download crates to all.sh

--- all.sh
+++ all.sh
@@ -3,6 +3,7 @@ set -e -u
 cd /home/gabor/work/rust-digger
 /home/gabor/.cargo/bin/cargo build --release > /tmp/rust-digger-build.log 2> /tmp/rust-digger-build.err
 ./target/release/rust-digger-download-db-dump > /tmp/rust-digger-download-db-dump.log 2> /tmp/rust-digger-download-db-dump.err
+./target/release/rust-digger-download-crates > /tmp/rust-digger-download-crates.log 2> /tmp/rust-digger-download-crates.err
 ./target/release/rust-digger-clone --recent 10 > /tmp/rust-digger-clone.log 2> /tmp/rust-digger-clone.err
 ./target/release/rust-digger-vcs > /tmp/rust-digger-vcs.log 2> /tmp/rust-digger-vcs.err
 ./target/release/rust-digger-html > /tmp/rust-digger-html.log 2> /tmp/rust-digger-html.err
